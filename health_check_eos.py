2ef8571eb511ba717546d42c3ced49d1d03c4dc4
test <li18618486935@outlook.com>
2026-05-20 17:28:09 +0800

    bump version to 1.4.1

--- health_check_eos.py
+++ health_check_eos.py
@@ -34,7 +34,7 @@ from typing import Callable, Dict, Iterable, List, Optional, Sequence, Tuple
 
 __author__ = "chris.li@arista.com"
 __last_modified__ = "2026-05-20"
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 
 LOG = logging.getLogger("health_check_eos")
